5ee587fd5ce4e8b109300ef993630d931ec8908d
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-01-16 23:06:50 +0500

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "@/pages/HomeView.vue";
 import AboutPage from "@/pages/AboutView.vue";
+import FaqPage from "@/pages/FaqView.vue";
+import LoginPage from "@/pages/LoginView.vue";
+import RegisterPage from "@/pages/RegisterView.vue";
 const routes = [
 	{
 		path: "/",
@@ -11,6 +14,21 @@ const routes = [
 		path: "/about",
 		name: "About",
 		component: AboutPage,
+	},
+	{
+		path: "/faq",
+		name: "Faq",
+		component: FaqPage,
+	},
+	{
+		path: "/login",
+		name: "Login",
+		component: LoginPage,
+	},
+	{
+		path: "/register",
+		name: "Register",
+		component: RegisterPage,
 	},
 ];
 
